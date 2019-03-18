6bad9ff2b090b970486eec98029f987c947d8f95
Choirul Anas <choirulanas2011@gmail.com>
2019-03-18 10:13:32 +0700

    add project module with 4 components

---
+++ angular7-practicetwo/src/app/admin/project-update/project-update.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-update',
+  templateUrl: './project-update.component.html',
+  styleUrls: ['./project-update.component.less']
+})
+export class ProjectUpdateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
